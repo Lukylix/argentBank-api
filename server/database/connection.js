b275eaf52cb830dd9f08de617bb9113a18e062ec
Lukylix <dev.garcia.lucas@gmail.com>
2022-11-23 21:08:41 +0100

    Update dependencies

--- server/database/connection.js
+++ server/database/connection.js
@@ -3,7 +3,7 @@ const databaseUrl = process.env.DATABASE_URL || "mongodb://localhost/argentBankD
 
 module.exports = async () => {
   try {
-    await mongoose.connect(databaseUrl, { useNewUrlParser: true });
+    await mongoose.connect(databaseUrl);
     console.log("Database successfully connected");
   } catch (error) {
     console.error(`Database Connectivity Error: ${error}`);
